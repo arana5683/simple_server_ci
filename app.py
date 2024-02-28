11c115270cb503c7d7823e8af189797b38dcd214
Aakash Rana <aakashrana.r4z@gmail.com>
2024-02-28 16:27:53 +0000

    Initial commit, complete server

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_hello():
+    return "Hello, world!"
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True, # Optional but useful for now
+        host="0.0.0.0" # Listen for connections directed _to_ any address
+    )
